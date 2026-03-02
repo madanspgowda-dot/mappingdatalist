618d835f57768957adad341b6d307fc3be471965
Madan Gowda S P <madanspgowda@gmail.com>
2026-03-02 09:51:23 +0530

    Initial commit

---
+++ src/main/java/com/example/mapingdatalist/MapingdatalistApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapingdatalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapingdatalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapingdatalistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mapingdatalist/MapingdatalistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapingdatalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapingdatalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
